72ab101699202b513e5fc8802b0e71009d381abc
praveenrvkl <praveenrvkl@gmail.com>
2024-06-01 18:06:38 +0530

    main code

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/praveenrvkl/animals
+
+go 1.19
+
+require github.com/praveenrvkl/puppy v0.0.0-20240601121550-917e0e04d0c7
+
+require github.com/praveenrvkl/dogs v0.0.0-20240601121235-bbc32396640a // indirect
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/praveenrvkl/puppy"
+)
+
+func main() {
+	fmt.Println("this is aninal")
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+
+	fmt.Println(puppy.BigBark())
+	fmt.Println(puppy.BigBarks())
+
+}
